53b7ec7b55181eb2622ced1910bec08599b4de5d
umr-33 <xx_yr@icloud.com>
2019-11-03 09:19:19 +0900

    インクリメンタルサーチ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
-  resources :users, only: [:edit, :update]
+  #root to: 'users#index'
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
