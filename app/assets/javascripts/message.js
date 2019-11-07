f276d480c6b04621375434a459976f9d2bfe2d27
umr-33 <xx_yr@icloud.com>
2019-11-07 11:39:47 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -8,7 +8,7 @@ $(function() {
               ${message.user_name}
             </div>
             <div class="upper-message__date">
-              ${message.date}
+              ${message.created_at}
             </div>
           </div>
           <div class="lower-meesage">
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content @message.content 
 json.image @message.image.url
-json.created_at @message.created_at
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.user_name @message.user.name
 json.id @message.id
