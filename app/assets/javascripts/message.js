ce1f5f3c2d86dff14691ecd009e7895f5ec4619d
umr-33 <xx_yr@icloud.com>
2019-11-07 13:14:39 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,29 +21,6 @@ $(function() {
     return html;
   
   }
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id"); 
-      $.ajax({
-        url: 'api/messages', 
-        type: 'get',
-        detatype: 'json',
-        data: {last_id: last_message_id}
-      })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function (message) {
-          insertHTML = buildHTML(message);
-          $('.messages').append(insertHTML);
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        })
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
-      });
-    }
-  };
-  setInterval(reloadMessages,5000);
 
   $('#new_message').on('submit', function(e){
     $("#new_message").prop('disabled', false);
@@ -70,4 +47,28 @@ $(function() {
       alert("メッセージ送信に失敗しました");
     });
   });
+  
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
+      $.ajax({
+        url: 'api/messages', 
+        type: 'get',
+        detatype: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages,5000);
 });
